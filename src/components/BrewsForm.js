157724b68908b033caeb47490bb287b7203a110f
Brooke Ross <brooke.ross2800@gmail.com>
2022-10-21 10:43:14 -0600

    some changes to form

--- src/components/BrewsForm.js
+++ src/components/BrewsForm.js
@@ -5,11 +5,12 @@ const BrewsForm = () => {
         <div className='brew-form-container'>
             <h1>BrewsForm</h1>
             <form className="brew-form">
-                <label>Name</label>
-                <input 
-                    type='text'
-                    placeholder='Name'
-                />
+                <label>Name:</label>
+                <input type='text'/>
+                <label>Image URL:</label>
+                <input type='text'/>
+                <label>Description:</label>
+                <input type='text'/>
                 <button className='submit-btn' type="submit">SUBMIT</button>
             </form>
         </div>
