695c49031fd25afa8ad0734d97bd74a8b772776d
Brooke Ross <brooke.ross2800@gmail.com>
2022-10-26 10:35:13 -0600

    finished search bar but messed up commit

--- src/components/GlobalBrewsFolder/GlobalBrews.js
+++ src/components/GlobalBrewsFolder/GlobalBrews.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import GlobalBrewsList from'./GlobalBrewsList'
 
-const GlobalBrews = ({currentId}) => {
+const GlobalBrews = ({currentId,search,setSearch}) => {
     
     return (
         <div className='brews-container'>
             <h1>Global Brews</h1>
-            <GlobalBrewsList currentId={currentId}/>
+            <GlobalBrewsList 
+                currentId={currentId}
+                search={search}
+                setSearch={setSearch}
+            />
         </div>
 
     )
--- src/components/MyBrewsFolder/MyBrews.js
+++ src/components/MyBrewsFolder/MyBrews.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import { Link } from 'react-router-dom';
 import BrewsList from'./BrewsList'
 
-const MyBrews = ({filterMyBrewsCards,id,handleDeleteBrew}) => {
+const MyBrews = ({search,setSearch,id,editFormData}) => {
+
+    const [myBrewsCards,setMyBrewsCards]=useState([])
+    
+    useEffect(()=>{
+        fetch(`http://localhost:9292/api/entries/user/${id}`)
+        .then((res) =>res.json())
+        .then(data => {
+            setSearch("")
+            setMyBrewsCards(data)
+        })
+      },[id, editFormData])
+
+    //Search bar logic for myBrews.  
+    const filterMyBrewsCards = myBrewsCards.filter((myBrewsCard) =>
+    myBrewsCard.name.toLowerCase().includes(search.toLowerCase())
+    )
+
+    //Logic for deleting a brew:
+    function handleDeleteBrew(deleteId){
+        const updatedBrewArray=myBrewsCards.filter((oneMyBrewCard) => oneMyBrewCard.id !== deleteId);
+        setMyBrewsCards(updatedBrewArray)
+    }
+
     
     return (
         <div className='brews-container'>
